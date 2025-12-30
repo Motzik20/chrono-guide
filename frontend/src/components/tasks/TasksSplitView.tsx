8d745c8c5a51cb17bd48e2b3d97dec9ff49d78fe
Motzik20 <ms983030@fh-muenster.de>
2025-12-30 16:03:33 +0100

    feat: make TaskSplitView modular

--- frontend/src/components/tasks/TasksSplitView.tsx
+++ frontend/src/components/tasks/TasksSplitView.tsx
@@ -1,18 +1,24 @@
 "use client";
 
-import IngestionInput from "./IngestionInput";
 import { Separator } from "@/components/ui/separator";
-import TaskDrafts from "./TaskDrafts";
 
-export default function TasksSplitView() {
+interface TasksSplitViewProps {
+  leftContent: React.ReactNode;
+  rightContent: React.ReactNode;
+}
+
+export default function TasksSplitView({
+  leftContent,
+  rightContent,
+}: TasksSplitViewProps) {
   return (
     <div className="flex w-full h-full flex-row">
       <div className="w-1/2 flex justify-center items-center p-4">
-        <IngestionInput />
+        {leftContent}
       </div>
       <Separator orientation="vertical" className="" />
       <div className="w-1/2 flex justify-center items-center p-4">
-        <TaskDrafts />
+        {rightContent}
       </div>
     </div>
   );
